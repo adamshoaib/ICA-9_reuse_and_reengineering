af91bd70d6513798921d6f0b3ee8a6175219c670
adamshoaibk <akareems@asu.edu>
2022-11-03 13:57:55 -0700

    Project Structure, added txt files, corrected method names

---
+++ src/test/java/AssignmentTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AssignmentTest {
+
+    @Test
+    void setDueDate() {
+    }
+
+    @Test
+    void setAssSpec() {
+    }
+
+    @Test
+    void isOverDue() {
+    }
+
+    @Test
+    void addSolution() {
+    }
+
+    @Test
+    void testAddSolution() {
+    }
+
+    @Test
+    void submitSolution() {
+    }
+
+    @Test
+    void getSolutionList() {
+    }
+
+    @Test
+    void getSolution() {
+    }
+
+    @Test
+    void getSugSolution() {
+    }
+
+    @Test
+    void getSolutionIterator() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getDueDateString() {
+    }
+
+    @Test
+    void accept() {
+    }
+}
